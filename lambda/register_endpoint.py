3318415ebb03b3ca512787582d5b4d930afcdc92
Lesego Rabotapi <rabotapilesego@gmail.com>
2026-01-29 13:53:38 +0200

    revert endpoint change

--- lambda/register_endpoint.py
+++ lambda/register_endpoint.py
@@ -58,15 +58,3 @@ def lambda_handler(event, context):
             }
         ),
     }
-
-    return {
-    "statusCode": 200,
-    "headers": {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "OPTIONS,POST"
-    },
-    "body": json.dumps({
-        "message": "Monitoring started successfully"
-    })
-}
